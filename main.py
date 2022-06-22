25309879fcb95e29ef80590cf0610de53902e300
zhuqinyong <18326657779@163.com>
2022-06-22 21:42:05 +0800

    添加实验内容

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ tm = tm1637.TM1637(clk=machine.Pin(14), dio=machine.Pin(15))
 led = machine.Pin(25, machine.Pin.OUT)
 buzzer = machine.Pin(0, machine.Pin.OUT)
 
-n = 10
+n = 100
 
 while True:
     led.toggle()
@@ -15,4 +15,4 @@ while True:
     tm.number(n)
     n = n - 1
     if n < 0:
-        n = 10
+        n = 100
